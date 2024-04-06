b182d35d0af80bdb2d3699e7d45ff0364bfbfba4
Mateusz Kochelski <mkochelski@student.agh.edu.pl>
2024-04-06 19:14:40 +0200

    Parsing from config file done.

--- ShoppingCartDistribution/src/main/java/com/ocado/basket/BasketSplitter.java
+++ ShoppingCartDistribution/src/main/java/com/ocado/basket/BasketSplitter.java
@@ -1,9 +1,28 @@
 package com.ocado.basket;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
 public class BasketSplitter {
 
+    private final List<Set<String>> couriersProductsMap;
 
+    public BasketSplitter(String absolutePathToConfigFile)  {
+        try
+        {
+            this.couriersProductsMap = JsonParser.parseConfigFile(absolutePathToConfigFile);
+        }
+        catch (IOException e)
+        {
+            System.out.println("COULD NOT FIND CONFIG FILE!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Map<String, List<String>> split(List<String> items){
+        return new HashMap<>();
+    }
 }
---
+++ ShoppingCartDistribution/src/main/java/com/ocado/basket/JsonParser.java
@@ -0,0 +1,35 @@
+package com.ocado.basket;
+
+import java.util.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.io.File;
+import java.io.IOException;
+public class JsonParser {
+
+
+    public static List<Set<String>> parseConfigFile(String absolutePathToConfigFile) throws IOException{
+        ObjectMapper objectMapper = new ObjectMapper();
+        HashMap<String, Set<String>> couriersProductsHashMap = new HashMap<>();
+        JsonNode rootNode = objectMapper.readTree(new File("C:\\Users\\mkoch\\Downloads\\Zadanie\\Zadanie\\config.json"));
+
+        for (Iterator<String> it = rootNode.fieldNames(); it.hasNext(); ) {
+            String product = it.next();
+
+            JsonNode deliveryMethodsNode = rootNode.get(product);
+            for (JsonNode deliveryMethod : deliveryMethodsNode)
+            {
+                if(!couriersProductsHashMap.containsKey(String.valueOf(deliveryMethod)))
+                    couriersProductsHashMap.put(String.valueOf(deliveryMethod), new HashSet<>());
+                couriersProductsHashMap.get(String.valueOf(deliveryMethod)).add(product);
+            }
+
+        }
+
+        List<Set<String>> couriersProductsMap = new ArrayList<>(couriersProductsHashMap.size());
+        couriersProductsMap.addAll(couriersProductsHashMap.values());
+
+        return couriersProductsMap;
+    }
+}
